ef2eb287901e17742c62b98c344594e143445e9f
hia1394 <a4737465@naver.com>
2025-10-19 22:01:10 +0900

    aicontroller

---
+++ src/main/java/com/click/click/controller/AiController.java
@@ -0,0 +1,12 @@
+package com.click.click.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+@RequiredArgsConstructor
+public class AiController {
+}
